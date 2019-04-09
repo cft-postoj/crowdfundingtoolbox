d3eb56f8894a6dc790f47a59c19f6f2920179fc5
kacmarik_stanislav <s.kacmarik@gmail.com>
2019-04-09 14:38:30 +0200

    global widget settings

--- dashboard-app/src/app/components/widget-settings/widget-settings.component.ts
+++ dashboard-app/src/app/components/widget-settings/widget-settings.component.ts
@@ -10,21 +10,39 @@ import {RadioButton} from "../../_parts/atoms/radio-button/radio-button";
 })
 export class WidgetSettingsComponent implements OnInit {
 
+    public paddingButtons: RadioButton[] = [];
+    public marginButtons: RadioButton[] = [];
+
+    public widget_settings={
+        padding: {
+            top: '20',
+            right: '15',
+            bottom: '35',
+            left: '15'
+        },
+        margin: {
+            top: '15',
+            right: 'auto',
+            bottom: '15',
+            left: 'auto'
+        },
+    }
+
     constructor(private router: Router) {
     }
 
     ngOnInit() {
         this.paddingButtons = [];
-        this.paddingButtons.push(new RadioButton("top", this.cta_widget.default.padding.top, "/assets/images/icons/padding_top.svg"))
-        this.paddingButtons.push(new RadioButton("right", this.cta_widget.default.padding.right, "/assets/images/icons/padding_right.svg"))
-        this.paddingButtons.push(new RadioButton("bottom", this.cta_widget.default.padding.bottom, "/assets/images/icons/padding_bottom.svg"))
-        this.paddingButtons.push(new RadioButton("left", this.cta_widget.default.padding.left, "/assets/images/icons/padding_left.svg"))
+        this.paddingButtons.push(new RadioButton("top", this.widget_settings.padding.top, "/assets/images/icons/padding_top.svg"))
+        this.paddingButtons.push(new RadioButton("right", this.widget_settings.padding.right, "/assets/images/icons/padding_right.svg"))
+        this.paddingButtons.push(new RadioButton("bottom", this.widget_settings.padding.bottom, "/assets/images/icons/padding_bottom.svg"))
+        this.paddingButtons.push(new RadioButton("left", this.widget_settings.padding.left, "/assets/images/icons/padding_left.svg"))
 
         this.marginButtons = [];
-        this.marginButtons.push(new RadioButton("top", this.cta_widget.default.margin.top, "/assets/images/icons/margin_top.svg"))
-        this.marginButtons.push(new RadioButton("right", this.cta_widget.default.margin.right, "/assets/images/icons/margin_right.svg"))
-        this.marginButtons.push(new RadioButton("bottom", this.cta_widget.default.margin.bottom, "/assets/images/icons/margin_bot.svg"))
-        this.marginButtons.push(new RadioButton("left", this.cta_widget.default.margin.left, "/assets/images/icons/margin_left.svg"))
+        this.marginButtons.push(new RadioButton("top", this.widget_settings.margin.top, "/assets/images/icons/margin_top.svg"))
+        this.marginButtons.push(new RadioButton("right", this.widget_settings.margin.right, "/assets/images/icons/margin_right.svg"))
+        this.marginButtons.push(new RadioButton("bottom", this.widget_settings.margin.bottom, "/assets/images/icons/margin_bot.svg"))
+        this.marginButtons.push(new RadioButton("left", this.widget_settings.margin.left, "/assets/images/icons/margin_left.svg"))
     }
 
     closeEditWindow() {
