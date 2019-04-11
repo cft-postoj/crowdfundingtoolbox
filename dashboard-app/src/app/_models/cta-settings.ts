c8a183dbd9049428037539d20d711794661b6842
Štefan Ondáš <ondas.stevo@gmail.com>
2019-04-11 08:29:45 +0200

    unversioned files

---
+++ dashboard-app/src/app/_models/cta-settings.ts
@@ -0,0 +1,81 @@
+export class CtaSettings {
+    default: any = {
+        padding: {
+            top: '15',
+            right: '15',
+            bottom: '15',
+            left: '15'
+        },
+        margin: {
+            top: '0',
+            right: 'auto',
+            bottom: '0',
+            left: 'auto'
+        },
+        fontSettings: {
+            fontFamily: 'Roboto Slab',
+            fontWeight: 'bold',
+            alignment: 'center',
+            color: '#FFFFFF',
+            fontSize: 24
+        },
+        design: {
+            fill: {
+                selected: true,
+                color: '#B71100',
+                opacity: 100
+            },
+            border: {
+                selected: false,
+                color: '#B71100',
+                size: 2,
+                opacity: 0
+            },
+            shadow: {
+                selected: false,
+                color: '#B71100',
+                x: 2,
+                y: 2,
+                b: 2,
+                opacity: 0
+            },
+            radius: {
+                selected: false,
+                value: '0'
+            }
+        }
+    };
+    hover: any = {
+        type: 'fade',
+        fontSettings: {
+            fontWeight: 'bold',
+            opacity: 100,
+            color: '#FFFFFF'
+        },
+        design: {
+            fill: {
+                selected: true,
+                color: '#B71100',
+                opacity: 100
+            },
+            border: {
+                selected: false,
+                color: '#B71100',
+                size: 2,
+                opacity: 0
+            },
+            shadow: {
+                selected: false,
+                color: '#B71100',
+                x: 2,
+                y: 2,
+                b: 2,
+                opacity: 0
+            },
+            radius: {
+                selected: false,
+                value: '0'
+            }
+        }
+    };
+}
