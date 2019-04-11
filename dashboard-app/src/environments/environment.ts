736c9ebb18189f773a36342913e12369124ec59e
kacmarik_stanislav <s.kacmarik@gmail.com>
2019-04-11 14:42:01 +0200

    preview - update

--- dashboard-app/src/environments/environment.ts
+++ dashboard-app/src/environments/environment.ts
@@ -2,7 +2,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-const googleApiKey = 'YOUR_API_KEY';
+const googleApiKey = 'AIzaSyA9p_o601-50SPOBkXFIkAnAzTx7RRn_UI';
 
 export const environment = {
 
