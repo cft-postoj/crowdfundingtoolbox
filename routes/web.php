794feb86a4cabe593e2e35465e72d533ef8d2127
Štefan Ondáš <ondas.stevo@gmail.com>
2019-04-18 17:08:55 +0200

    Add login view visual

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/portal/login', function() {
+   return view('portal-templates.login');
+});
 
 Route::any('{slug}', function($slug)
 {
