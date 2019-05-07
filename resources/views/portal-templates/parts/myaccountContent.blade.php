bb0045d52fa283deccff71d1e20f570985ba7b7d
stefan.ondas <stefan.ondas@proup.sk>
2019-05-07 11:24:17 +0200

    my account wireframe section

---
+++ resources/views/portal-templates/parts/myaccountContent.blade.php
@@ -0,0 +1,60 @@
+<!-- MY ACCOUNT PART SECTION, you can customize it like you want. Please, don't change used classes and identificators. -->
+<div class="container">
+    <div class="cft--myAccount">
+        <div class="cft--myAccount--sidebar">
+            <ul>
+                <li>
+                    <button type="button"  id="showCftNewslettersSection">Newslettre</button>
+                </li>
+                <li>
+                    <button type="button" id="showCftSavedArticlesSection">Uložené články</button>
+                </li>
+                <li>
+                    <button type="button" id="showCftDonationSection">Podpora</button>
+                </li>
+                <li>
+                    <button type="button" id="showCftMyProfileSection">Môj profil</button>
+                </li>
+                <li>
+                    <button type="button" id="showCftMyOrdersSection">Moje objednávky</button>
+                </li>
+            </ul>
+        </div>
+        <div class="cft--myAccount--body">
+            <div class="cft--myAccount--body--intro">
+                <h2>Vitajte vo svojom účte</h2>
+                <p>Lorem ipsum dolor...</p>
+            </div>
+            <div id="cft--myAccount--newsletters" class="cft--myAccount--body--section">
+                <h2>Newslettre</h2>
+                <p>Lorem ipsum dolor...</p>
+            </div>
+            <div id="cft--myAccount--savedArticles" class="cft--myAccount--body--section">
+                <h2>Uložené články</h2>
+                <p>Lorem ipsum dolor...</p>
+            </div>
+            <div id="cft--myAccount--donation" class="cft--myAccount--body--section">
+                <h2>Podpora</h2>
+                <p>Lorem ipsum dolor...</p>
+            </div>
+            <div id="cft--myAccount--myProfile" class="cft--myAccount--body--section">
+                <h2>Môj profil</h2>
+                <p>Lorem ipsum dolor...</p>
+                <form>
+                    <label for="cft--form--meno">Meno:</label>
+                    <input id="cft--form--meno" type="text">
+                    <label for="cft--form--priezvisko">Priezvisko:</label>
+                    <input id="cft--form--priezvisko" type="text">
+                    <label for="cft--form--email">Email:</label>
+                    <input id="cft--form--email" type="email">
+                    <label for="cft--form--telefon">Telefón:</label>
+                    <input id="cft--form--telefon" type="text">
+                </form>
+            </div>
+            <div id="cft--myAccount--myOrders" class="cft--myAccount--body--section">
+                <h2>Moje objednávky</h2>
+                <p>Lorem ipsum dolor...</p>
+            </div>
+        </div>
+    </div>
+</div>
