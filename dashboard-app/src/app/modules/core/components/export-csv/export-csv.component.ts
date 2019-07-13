1ad75173ba87ba7004d1f10724cac38c6da875e1
Štefan Ondáš <stefan.ondas@proup.sk>
2019-07-13 14:47:34 +0200

    update component logic export csv

--- dashboard-app/src/app/modules/core/components/export-csv/export-csv.component.ts
+++ dashboard-app/src/app/modules/core/components/export-csv/export-csv.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {ExportCsvService} from '../../services/export-csv.service';
 
 @Component({
-  selector: 'app-export-csv',
-  templateUrl: './export-csv.component.html',
-  styleUrls: ['./export-csv.component.scss']
+    selector: 'app-export-csv',
+    templateUrl: './export-csv.component.html',
+    styleUrls: ['./export-csv.component.scss']
 })
 export class ExportCsvComponent implements OnInit {
 
-  constructor() { }
+    @Input()
+    public title: string;
+    @Input()
+    public data: any = [];
+    @Input()
+    public columnNames: any = {};
+    @Output()
+    public resultEmitter = new EventEmitter();
 
-  ngOnInit() {
-  }
+    constructor(private exportCsvService: ExportCsvService) {
+    }
+
+    ngOnInit() {
+    }
+
+    public export() {
+        let emitData = {};
+        if (this.data[0].length !== this.columnNames.length) {
+            emitData = {
+                alertType: 'danger',
+                alertMessage: 'Incorrectly defined inputs. Count of titles is not same with count of data items.',
+                alertOpen: true
+            };
+            return this.resultEmitter.emit(emitData);
+        } else {
+            this.exportCsvService.makeExport(this.title, this.data, this.columnNames).subscribe((d) => {
+                console.log(d);
+                emitData = {
+                    alertType: 'success',
+                    alertMessage: 'Export was successfully created.',
+                    alertOpen: true
+                };
+            }, (error) => {
+                console.log(error);
+                emitData = {
+                    alertType: 'danger',
+                    alertMessage: 'There was an error during export creating. ERROR: ' + error,
+                    alertOpen: true
+                };
+            }, () => {
+                return this.resultEmitter.emit(emitData);
+            });
+        }
+    }
 
 }
--- dashboard-app/src/app/modules/core/services/export-csv.service.ts
+++ dashboard-app/src/app/modules/core/services/export-csv.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ExportCsvService {
 
-  constructor() { }
+    constructor(private http: HttpClient) {
+    }
+
+    public makeExport(title: string, data: any, columnNames: any) {
+        return this.http.post(`${environment.backOfficeUrl}${environment.exportCsv}`, {
+            title: title,
+            data: data,
+            columnNames: columnNames
+        });
+    }
 }
