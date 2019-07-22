f79e337712d731de1a5988fb48a51cb368c12185
Štefan Ondáš <stefan.ondas@proup.sk>
2019-07-22 08:06:50 +0200

    add migration with remove article link widget (article widget will be replaced this)

--- backend/Modules/Campaigns/Entities/WidgetType.php
+++ backend/Modules/Campaigns/Entities/WidgetType.php
@@ -3,8 +3,10 @@
 namespace Modules\Campaigns\Entities;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class WidgetType extends Model
 {
+    use SoftDeletes;
     protected $table = 'widget_types';
 }
